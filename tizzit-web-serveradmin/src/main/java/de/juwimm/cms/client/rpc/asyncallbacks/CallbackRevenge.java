448d57e6de85424cb298f3aca41881030e706778
Sascha-Matthias Kulawik <sascha@kulawik.de>
2009-07-30 14:35:32 +0000

    Changed names in poms
    git-svn-id: http://tizzit.googlecode.com/svn/trunk@9 e03753de-7d0d-11de-8963-3b09bc248160

--- tizzit-web-serveradmin/src/main/java/de/juwimm/cms/client/rpc/asyncallbacks/CallbackRevenge.java
+++
@@ -1,6 +0,0 @@
-package de.juwimm.cms.client.rpc.asyncallbacks;
-
-
-public interface CallbackRevenge <T>{
-	public void callback(T result, String clazzName);
-}
--- tizzit-web-serveradmin/src/main/java/de/juwimm/cms/client/view/layoutcontainer/LogFileParserPanel.java
+++
@@ -1,7 +0,0 @@
-package de.juwimm.cms.client.view.layoutcontainer;
-
-import com.extjs.gxt.ui.client.widget.LayoutContainer;
-
-public class LogFileParserPanel extends LayoutContainer {
-
-}
