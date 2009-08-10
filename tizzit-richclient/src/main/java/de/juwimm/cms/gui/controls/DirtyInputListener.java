3a8f710d0146eee15565201de270e2e83ca31cf9
Florin Zalum <florin.zalum@gmail.com>
2009-08-10 07:16:15 +0000

    add comments
    git-svn-id: http://tizzit.googlecode.com/svn/trunk@28 e03753de-7d0d-11de-8963-3b09bc248160

--- tizzit-richclient/src/main/java/de/juwimm/cms/gui/controls/DirtyInputListener.java
+++ tizzit-richclient/src/main/java/de/juwimm/cms/gui/controls/DirtyInputListener.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2009 Juwi MacMillan Group GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.juwimm.cms.gui.controls;
 
 import java.awt.event.ItemEvent;
@@ -18,9 +33,13 @@ import javax.swing.event.TableModelListener;
 /**
  * @author <a href="florin.zalum@juwimm.com">Florin Zalum</a>
  * @version $Id$
+ * Used for different control types(JTextField,JComboBox,JSpinner,JTable) to detect value changes.
  */
 public abstract class DirtyInputListener{
 	
+	/**
+	 * Called when a control changes his value.
+	 */
 	public abstract void onChangeValue();
 	
 	public static void addDirtyInputListener(JComponent component,final DirtyInputListener listener){		
