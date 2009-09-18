81bd9c51326ad9f7783ea63448f04061c2d88d18
Florin Zalum <florin.zalum@gmail.com>
2009-09-18 14:34:20 +0000

    key for date formats
    git-svn-id: http://tizzit.googlecode.com/svn/trunk@221 e03753de-7d0d-11de-8963-3b09bc248160

--- tizzit-richclient/src/main/java/de/juwimm/cms/gui/controls/DateChooser.java
+++ tizzit-richclient/src/main/java/de/juwimm/cms/gui/controls/DateChooser.java
@@ -2,17 +2,18 @@ package de.juwimm.cms.gui.controls;
 
 import com.toedter.calendar.JDateChooser;
 
+import static de.juwimm.cms.common.Constants.rb;
 import de.juwimm.cms.util.UIConstants;
 
 /**
  * @author <a href="florin.zalum@juwimm.com">Florin Zalum</a>
- * @version $Id
+ * @version $Id$
  */
 public class DateChooser extends JDateChooser{
 	private static final long serialVersionUID = 3122411689996068892L;
 
 	public DateChooser(){
-		super(new CalendarChooser(), null,"dd/MM/yyyy", null);
+		super(new CalendarChooser(), null,rb.getString("dateChooser.format"), null);
 		this.calendarButton.setIcon(UIConstants.ICON_CALENDAR);
 	}
 }
