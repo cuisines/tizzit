ab8ab4fb5ef295756458664dacc7caccbb988d1d
Eduard Siebert <eduard.siebert@online.de>
2009-10-09 00:47:34 +0000

    TIZZIT-167 - renamed tizzit-plugins.properties to tizzit-plugins.conf and added a parser for it
    git-svn-id: http://tizzit.googlecode.com/svn/trunk@405 e03753de-7d0d-11de-8963-3b09bc248160

--- tizzit-core/src/main/java/de/juwimm/cms/beans/PluginManagement.java
+++ tizzit-core/src/main/java/de/juwimm/cms/beans/PluginManagement.java
@@ -15,13 +15,16 @@
  */
 package de.juwimm.cms.beans;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Date;
-import java.util.Properties;
+import java.util.HashMap;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.tizzit.core.classloading.ClassloadingHelper;
 import org.xml.sax.Attributes;
@@ -34,7 +37,7 @@ import de.juwimm.cms.plugins.server.Response;
 import de.juwimm.cms.plugins.server.TizzitPlugin;
 
 /**
- * 
+ *
  * @author <a href="mailto:j2ee@juwimm.com">Sascha-Matthias Kulawik</a>
  * company Juwi|MacMillan Group GmbH, Walsrode, Germany
  * @version $Id$
@@ -42,42 +45,32 @@ import de.juwimm.cms.plugins.server.TizzitPlugin;
  */
 public class PluginManagement {
 	private Logger log = Logger.getLogger(PluginManagement.class);
-	private Properties props = null;
+	private PluginConfig<String, String> pluginConfig = null;
 
-	public PluginManagement() {		
-		try {
-			URL cp = this.getClass().getResource("/tizzit-plugins.properties");
-			InputStream is = new FileInputStream(cp.getFile());
-			props = new Properties();
-			is = new FileInputStream(cp.getFile());
-			props.load(is);
-		} catch (FileNotFoundException e) {
-			log.error("Could not find tizzit-plugins.properties - no plugins will be available!");
-		} catch (Exception e) {
-			log.error("Could not find tizzit-plugins.properties - no plugins will be available!");
-		}
+	public PluginManagement() {
+		this.pluginConfig = new PluginConfig<String, String>();
 	}
 
 	public TizzitPlugin getPlugin(String namespace) {
 		TizzitPlugin plugin = null;
-		String clazzName = props.getProperty(namespace);
-		if(clazzName == null) {
+		String clazzName = this.pluginConfig.get(namespace);
+		if (clazzName == null) {
 			log.warn("Could not find Plugin for Namespace: " + namespace + "!");
 			plugin = new NullPlugin();
 		} else {
 			try {
 				plugin = (TizzitPlugin) ClassloadingHelper.getInstance(clazzName);
-			} catch(Exception exe) {
+			} catch (Exception exe) {
 				log.warn("Could not resolve TizzitPlugin with classname " + clazzName + " " + exe.getMessage());
 				plugin = new NullPlugin();
 			}
 		}
 		return plugin;
 	}
-	
+
 	public class NullPlugin implements TizzitPlugin {
 		ContentHandler parent = null;
-		
+
 		public void configurePlugin(Request request, Response response, ContentHandler parent, Integer uniquePageId) {
 			this.parent = parent;
 		}
@@ -135,6 +128,106 @@ public class PluginManagement {
 
 		public void startPrefixMapping(String arg0, String arg1) throws SAXException {
 			parent.startPrefixMapping(arg0, arg1);
+		}
+	}
+
+	/**
+	 *
+	 * @author <a href="mailto:eduard.siebert@juwimm.com">Eduard Siebert</a>
+	 * company Juwi MacMillan Group GmbH, Walsrode, Germany
+	 * @version $Id$
+	 * @since tizzit-core 09.10.2009
+	 * @param <K>
+	 * @param <V>
+	 */
+	public class PluginConfig<K, V> extends HashMap<String, String> {
+		private static final long serialVersionUID = -2484247980813323117L;
+
+		public PluginConfig() {
+			super();
+			this.parsePluginConfig();
+		}
+
+		private void parsePluginConfig() {
+			if (log.isDebugEnabled()) log.debug("parsePluginConfig() -> begin");
+			try {
+				URL cp = this.getClass().getResource("/tizzit-plugins.conf");
+				InputStream is = new FileInputStream(cp.getFile());
+				InputStreamReader reader = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(reader);
+
+				String line = br.readLine();
+				int lineNumber = 1;
+				while (line != null) {
+					String formedLine = StringUtils.deleteWhitespace(line);
+					if (!formedLine.startsWith("#")) {
+						try {
+							int sepIndex = formedLine.lastIndexOf("=");
+							int commentIndex = formedLine.indexOf("#");
+							if (sepIndex > -1) {
+								String namespace = null;
+								String clazzName = null;
+								if (commentIndex > sepIndex) {
+									// entries with comments  behind the equal char
+									namespace = formedLine.substring(0, sepIndex);
+									clazzName = formedLine.substring(sepIndex + 1, commentIndex);
+								} else if (commentIndex == -1) {
+									// entries without comments on line
+									namespace = formedLine.substring(0, sepIndex);
+									clazzName = formedLine.substring(sepIndex + 1);
+								} else {
+									log.warn(appendLineNumber("Ignoring entry: '" + line + "' because the comment char is before the equals sign!", lineNumber));
+								}
+								namespace = StringUtils.trimToNull(namespace);
+								clazzName = StringUtils.trimToNull(clazzName);
+								if (namespace != null && clazzName != null) {
+									if (!this.containsKey(namespace)) {
+										this.put(namespace, clazzName);
+										log.info(appendLineNumber("Mapped plugin namespace '" + namespace + "' to class '" + clazzName + "'.", lineNumber));
+									} else {
+										log.warn(appendLineNumber("Ignoring plugin namespace '" + namespace + "' because it has already been added.", lineNumber));
+									}
+								} else {
+									log.warn(appendLineNumber("Namespace and class MUST not be null! Ignoring...", lineNumber));
+								}
+							}
+						} catch (Exception exe) {
+							log.error(appendLineNumber("Could not parse line '" + formedLine + "': " + exe.getMessage(), lineNumber));
+						}
+					} else {
+						if (log.isDebugEnabled()) log.debug(appendLineNumber("Ignoring entry: '" + line + "' because it is a comment!", lineNumber));
+					}
+					line = br.readLine();
+					lineNumber++;
+				}
+			} catch (FileNotFoundException e) {
+				log.error("Could not find tizzit-plugins.conf - no plugins will be available!");
+			} catch (Exception e) {
+				log.error("Could not find tizzit-plugins.conf - no plugins will be available!");
+			}
+
+			if (log.isDebugEnabled()) {
+				StringBuffer resultMsg = new StringBuffer();
+				resultMsg.append("\nResult {\n");
+				resultMsg.append("\tSize: " + this.size() + "\n");
+				if (this.size() > 1) {
+					resultMsg.append("\tEntries:\n");
+				} else {
+					resultMsg.append("\tEntry:\n");
+				}
+				for (String key : this.keySet()) {
+					resultMsg.append("\t\tNamespace: " + key + "\n");
+					resultMsg.append("\t\tClass    : " + this.get(key) + "\n");
+				}
+				resultMsg.append("}");
+				log.debug(resultMsg.toString());
+			}
+
+			if (log.isDebugEnabled()) log.debug("parsePluginConfig() -> end");
+		}
+
+		private String appendLineNumber(String message, int lineNumber) {
+			return message + " [Line: " + lineNumber + "]";
 		}
 	}
 }
