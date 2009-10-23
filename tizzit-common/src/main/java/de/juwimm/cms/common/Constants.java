056c31bd0b9ee16f93b017c5a219259f39b55b70
Dan Iftimi <dan.iftimi@juwimm.com>
2009-10-23 17:00:59 +0000

    Tizzit-72
    git-svn-id: http://tizzit.googlecode.com/svn/trunk@546 e03753de-7d0d-11de-8963-3b09bc248160

--- tizzit-common/src/main/java/de/juwimm/cms/common/Constants.java
+++ tizzit-common/src/main/java/de/juwimm/cms/common/Constants.java
@@ -210,8 +210,9 @@ public final class Constants {
 	public static final String PROPERTY_PROPAGATION = "PROPERTY_PROPAGATION";
 	public static final String PROPERTY_DEPROPAGATION = "PROPERTY_DEPROPAGATION";
 	public static final String PROPERTY_CONFIGURATION = "PROPERTY_CONFIGURATION";
-	
+
 	public static final String ACTION_MAKE_VIEW_OFFLINE = "makeViewOffline";
+	public static Integer VIEW_COMPONENT_TO_COPY = null;
 
 	public static final int ADD_BEFORE = 0;
 	public static final int ADD_AFTER = 1;
