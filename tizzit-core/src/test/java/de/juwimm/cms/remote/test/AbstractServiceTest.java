3a30fb85adff941d427c37dc12c9b30f2e48bd80
Florin Zalum <florin.zalum@gmail.com>
2009-12-02 12:59:06 +0000

    TIZZIT-281 Mock the security context for service tests
    git-svn-id: http://tizzit.googlecode.com/svn/trunk@664 e03753de-7d0d-11de-8963-3b09bc248160

---
+++ tizzit-core/src/test/java/de/juwimm/cms/remote/test/AbstractServiceTest.java
@@ -0,0 +1,27 @@
+package de.juwimm.cms.remote.test;
+
+import junit.framework.TestCase;
+
+import org.springframework.security.Authentication;
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.GrantedAuthorityImpl;
+import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.context.SecurityContextImpl;
+import org.springframework.security.providers.anonymous.AnonymousAuthenticationToken;
+
+/**
+ * @author <a href="florin.zalum@juwimm.com">Florin Zalum</a>
+ * @version $Id$
+ */
+public class AbstractServiceTest extends TestCase {
+
+	@Override
+	protected void setUp() throws Exception {
+		//Mocking the security context
+		//Now AuthenticationHelper.getUserName will return "testUser"
+		SecurityContextImpl secureContext = new SecurityContextImpl();
+		Authentication token = new AnonymousAuthenticationToken("testUser", "testUser", new GrantedAuthority[] {new GrantedAuthorityImpl("testRole")});
+		secureContext.setAuthentication(token);
+		SecurityContextHolder.setContext(secureContext);
+	}
+}
