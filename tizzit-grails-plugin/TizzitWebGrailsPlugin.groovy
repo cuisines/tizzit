29dcd30b189b230f71dddac04e8379601075eb90
Holger Schinzel <holger.schinzel@gmx.de>
2010-08-10 13:44:29 +0000

    added maven build system
    git-svn-id: http://tizzit.googlecode.com/svn/trunk@883 e03753de-7d0d-11de-8963-3b09bc248160

--- tizzit-grails-plugin/TizzitWebGrailsPlugin.groovy
+++ tizzit-grails-plugin/TizzitWebGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class TizzitWebGrailsPlugin {
     // the plugin version
     def version = "0.2"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.3 > *"
+    def grailsVersion = "1.3.2 > *"
     // the other plugins this plugin depends on
     def dependsOn = [rest:"* > 0.3"]
     // resources that are excluded from plugin packaging
